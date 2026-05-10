bf48e68fa56c6b60ba773f53296b92a46b7eef04
Kiransai217 <kiransaikasani217@gmail.com>
2026-05-10 13:41:57 +0530

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,15 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        int temp = x;
+        int rev = 0;
+        while(temp >0){
+            int digit = temp%10;
+            rev = (rev*10)+digit;
+            temp /= 10;
+        }
+        if(rev == x){
+            return true;
+        }
+        return false;
+    }
+}
