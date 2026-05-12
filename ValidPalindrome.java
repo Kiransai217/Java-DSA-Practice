3bb9ffa019cf332ab27b11dc5f370813c9d1ba12
Kiransai217 <kiransaikasani217@gmail.com>
2026-05-12 19:21:11 +0530

    Added LeetCode Valid Palindrome Solution in java

---
+++ ValidPalindrome.java
@@ -0,0 +1,15 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String temp = "";
+        for(int i=0; i<s.length(); i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                temp += s.charAt(i);
+            }
+        }
+        String temp2 = "";
+        for(int i=temp.length()-1; i>=0; i--){
+            temp2 += temp.charAt(i);
+        }
+        return temp.toLowerCase().equals(temp2.toLowerCase());
+    }
+}
