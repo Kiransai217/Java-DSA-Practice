855c9c931992dedf2ddf38f5ca545284fd4efec4
Kiransai217 <kiransaikasani217@gmail.com>
2026-05-18 16:09:56 +0530

    Added LeetCode Contains Duplicate solution in java

---
+++ ContainsDuplicate.java
@@ -0,0 +1,11 @@
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> map =new HashSet<>();
+        for(int num : nums){
+            if(!map.add(num)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
